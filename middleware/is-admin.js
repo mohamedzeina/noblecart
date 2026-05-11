985a9daf7c143496c6a0a3ed9e7c1772c1e140c6
Mohamed Zeina <toxiczeina@gmail.com>
2026-05-11 13:33:22 +0300

    fix: improve isAdmin middleware to redirect unauthenticated and block customers

--- middleware/is-admin.js
+++ middleware/is-admin.js
@@ -1,4 +1,7 @@
 module.exports = (req, res, next) => {
+  if (req.admin === undefined && req.user === undefined) {
+    return res.redirect('/admin/login');
+  }
   if (req.admin === undefined) {
     return res.status(403).render('403', { pageTitle: 'Access Denied', path: '' });
   }
